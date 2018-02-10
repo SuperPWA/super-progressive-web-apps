38e13300c2db7f3c5698aa4bb37c0d10ad4febea
Arun Basil Lal <arunbasillal@gmail.com>
2018-02-10 10:13:34 +0530

    Added revving strategy

--- public/sw.php
+++ public/sw.php
@@ -111,6 +111,19 @@ self.addEventListener('fetch', function(e) {
 		);
 		return;
 	}
+	
+	// Revving strategy
+	if ( e.request.mode === 'navigate' && navigator.onLine ) {
+		e.respondWith(
+			fetch(e.request).then(function(response) {
+				return caches.open(cacheName).then(function(cache) {
+					cache.put(e.request, response.clone());
+					return response;
+				});  
+			})
+		);
+		return;
+	}
 
 	e.respondWith(
 		caches.match(e.request).then(function(response) {
