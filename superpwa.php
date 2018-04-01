2f5386b17624056b26c12f3e145b49dc62bfb7ad
Arun Basil Lal <arunbasillal@gmail.com>
2018-04-01 11:43:27 +0530

    Updated reference to loader.php and fomatting changes

--- superpwa.php
+++ superpwa.php
@@ -13,25 +13,26 @@
  */
 
 /**
- * This plugin was developed using the WordPress starter plugin template by Arun Basil Lal <arunbasillal@gmail.com>
- * Please leave this credit and the directory structure intact for future developers who might read the code. 
- * @Github https://github.com/arunbasillal/WordPress-Starter-Plugin
- */
- 
-/**
  * ~ Directory Structure ~
  *
- * /admin/ 						- Plugin backend stuff.
+ * Based on the WordPress starter plugin template
+ * @link https://github.com/arunbasillal/WordPress-Starter-Plugin
+ *
+ * /3rd-party/					- Functions for compatibility with 3rd party plugins and hosts.
+ * /admin/ 						- Plugin backend.
+ * /functions/					- Functions and utilites.
  * /includes/					- External third party classes and libraries.
  * /languages/					- Translation files go here. 
  * /public/						- Front end files go here.
  * index.php					- Dummy file.
  * license.txt					- GPL v2
- * superpwa.php					- File containing plugin name and other version info for WordPress.
- * readme.txt					- Readme for WordPress plugin repository. https://wordpress.org/plugins/files/2017/03/readme.txt
+ * loader.php					- Loads everything.
+ * superpwa.php					- Main plugin file.
+ * README.MD					- Readme for GitHub.
+ * readme.txt					- Readme for WordPress plugin repository.
  * uninstall.php				- Fired when the plugin is uninstalled. 
  */
- 
+
 /**
  * ~ Release TODO ~
  *
@@ -47,18 +48,18 @@ if ( ! defined('ABSPATH') ) exit;
  *
  * @since 		1.0
  */
-if ( ! defined('SUPERPWA_PATH_ABS ') ) 			define('SUPERPWA_PATH_ABS', plugin_dir_path( __FILE__ )); // absolute path to the plugin directory. eg - /var/www/html/wp-content/plugins/superpwa/
-if ( ! defined('SUPERPWA_PATH_SRC') ) 			define('SUPERPWA_PATH_SRC', plugin_dir_url( __FILE__ )); // link to the plugin folder. eg - http://example.com/wp/wp-content/plugins/superpwa/
-if ( ! defined('SUPERPWA_VERSION') ) 			define('SUPERPWA_VERSION', '1.6'); // Plugin version
-if ( ! defined('SUPERPWA_MANIFEST_FILENAME') ) 	define('SUPERPWA_MANIFEST_FILENAME', 'superpwa-manifest.json'); // Name of Manifest file
-if ( ! defined('SUPERPWA_MANIFEST_ABS') )		define('SUPERPWA_MANIFEST_ABS', trailingslashit( ABSPATH ) . SUPERPWA_MANIFEST_FILENAME); // Absolute path to manifest
-if ( ! defined('SUPERPWA_MANIFEST_SRC') )		define('SUPERPWA_MANIFEST_SRC', trailingslashit( get_bloginfo('wpurl') ) . SUPERPWA_MANIFEST_FILENAME); // Link to manifest
-if ( ! defined('SUPERPWA_SW_FILENAME') ) 		define('SUPERPWA_SW_FILENAME', 'superpwa-sw.js'); // Name of service worker file
-if ( ! defined('SUPERPWA_SW_ABS') )				define('SUPERPWA_SW_ABS', trailingslashit( ABSPATH ) . SUPERPWA_SW_FILENAME); // Absolute path to service worker. SW must be in the root folder
-if ( ! defined('SUPERPWA_SW_SRC') )				define('SUPERPWA_SW_SRC', trailingslashit( get_bloginfo('wpurl') ) . SUPERPWA_SW_FILENAME); // Link to service worker
+if ( ! defined('SUPERPWA_VERSION') ) 			define('SUPERPWA_VERSION'			, '1.6'); // Plugin version
+if ( ! defined('SUPERPWA_PATH_ABS ') ) 			define('SUPERPWA_PATH_ABS'			, plugin_dir_path( __FILE__ )); // Absolute path to the plugin directory. eg - /var/www/html/wp-content/plugins/super-progressive-web-apps/
+if ( ! defined('SUPERPWA_PATH_SRC') ) 			define('SUPERPWA_PATH_SRC'			, plugin_dir_url( __FILE__ )); // Link to the plugin folder. eg - http://example.com/wp/wp-content/plugins/super-progressive-web-apps/
+if ( ! defined('SUPERPWA_MANIFEST_FILENAME') ) 	define('SUPERPWA_MANIFEST_FILENAME'	, 'superpwa-manifest.json'); // Name of Manifest file
+if ( ! defined('SUPERPWA_MANIFEST_ABS') )		define('SUPERPWA_MANIFEST_ABS'		, trailingslashit( ABSPATH ) . SUPERPWA_MANIFEST_FILENAME); // Absolute path to manifest
+if ( ! defined('SUPERPWA_MANIFEST_SRC') )		define('SUPERPWA_MANIFEST_SRC'		, trailingslashit( get_bloginfo('wpurl') ) . SUPERPWA_MANIFEST_FILENAME); // Link to manifest
+if ( ! defined('SUPERPWA_SW_FILENAME') ) 		define('SUPERPWA_SW_FILENAME'		, 'superpwa-sw.js'); // Name of service worker file
+if ( ! defined('SUPERPWA_SW_ABS') )				define('SUPERPWA_SW_ABS'			, trailingslashit( ABSPATH ) . SUPERPWA_SW_FILENAME); // Absolute path to service worker. SW must be in the root folder
+if ( ! defined('SUPERPWA_SW_SRC') )				define('SUPERPWA_SW_SRC'			, trailingslashit( get_bloginfo('wpurl') ) . SUPERPWA_SW_FILENAME); // Link to service worker
 
 // Load everything
-require_once( SUPERPWA_PATH_ABS . 'admin/loader.php');
+require_once( SUPERPWA_PATH_ABS . 'loader.php');
 
 // Register activation hook (this has to be in the main plugin file.)
 register_activation_hook( __FILE__, 'superpwa_activate_plugin' );
