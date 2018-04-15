96e26b99b2392ca6cf5cb5038c84de57d07edbf1
Arun Basil Lal <arunbasillal@gmail.com>
2018-04-16 00:43:53 +0530

    New File: Added multisite.php to /functions/

---
+++ functions/multisite.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Functions for compatibility with WordPress multisites
+ *
+ * @since 1.6
+ * @function	superpwa_multisite_filename_postfix()	 Filename postfix for multisites
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Filename postfix for multisites
+ * 
+ * @return String Returns the current blog ID on a multisite. An empty string otherwise
+ * @since 1.6
+ */
+superpwa_multisite_filename_postfix() {
+	
+	// Return empty string if not a multisite
+	if ( ! is_multisite() ) {
+		return '';
+	}
+	
+	return '-' . get_current_blog_id();
+}
--- loader.php
+++ loader.php
@@ -9,17 +9,18 @@
 if ( ! defined('ABSPATH') ) exit;
 
 // Load admin
-require_once( SUPERPWA_PATH_ABS . 'admin/basic-setup.php');
-require_once( SUPERPWA_PATH_ABS . 'admin/admin-setup.php');
-require_once( SUPERPWA_PATH_ABS . 'admin/admin-ui-render.php');
+require_once( SUPERPWA_PATH_ABS . 'admin/basic-setup.php' );
+require_once( SUPERPWA_PATH_ABS . 'admin/admin-setup.php' );
+require_once( SUPERPWA_PATH_ABS . 'admin/admin-ui-render.php' );
 
 // 3rd party compatibility
-require_once( SUPERPWA_PATH_ABS . '3rd-party/onesignal.php');
+require_once( SUPERPWA_PATH_ABS . '3rd-party/onesignal.php' );
 
 // Load functions
-require_once( SUPERPWA_PATH_ABS . 'functions/common.php');
-require_once( SUPERPWA_PATH_ABS . 'functions/filesystem.php');
+require_once( SUPERPWA_PATH_ABS . 'functions/common.php' );
+require_once( SUPERPWA_PATH_ABS . 'functions/filesystem.php' );
+require_once( SUPERPWA_PATH_ABS . 'functions/multisite.php' );
 
 // Public folder
-require_once( SUPERPWA_PATH_ABS . 'public/manifest.php');
-require_once( SUPERPWA_PATH_ABS . 'public/sw.php');
+require_once( SUPERPWA_PATH_ABS . 'public/manifest.php' );
+require_once( SUPERPWA_PATH_ABS . 'public/sw.php' );
