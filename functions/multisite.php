4010469612acd0b1201b2a9f636303c4b9054953
Arun Basil Lal <arunbasillal@gmail.com>
2018-04-22 14:24:39 +0530

    Multisite: Handling deactivation cases where the blog is still using the default settings
    If SuperPWA is deactivated and if the blog is using the default settings (i.e, settings not saved to database yet).
    
    SuperPWA ships with smart defaults. Some blogs might not change the default settings.
    Default settings are not saved to the database until the user clicks the "Save Settings" in the plugin UI. (refer superpwa_get_settings() )
    If that is the case, there is nothing more to clean up during uninstall, and we can remove the blog id from our list.
    
    If settings are saved in the database, we want to preserve it, in case the user activates the plugin again.
    These settings are removed during uninstall.

--- functions/multisite.php
+++ functions/multisite.php
@@ -13,7 +13,8 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 /**
  * Filename postfix for multisites
  * 
- * @return String Returns the current blog ID on a multisite. An empty string otherwise
+ * @return (string) Returns the current blog ID on a multisite. An empty string otherwise
+ * 
  * @since 1.6
  */
 function superpwa_multisite_filename_postfix() {
@@ -33,7 +34,8 @@ function superpwa_multisite_filename_postfix() {
  * Manifest and service worker are deleted during deactivation. 
  * Database settings are cleaned during uninstall
  *
- * @param $status Boolean True when plugin is activated, false when deactivated
+ * @param (bool) $status True when plugin is activated, false when deactivated.
+ * 
  * @since 1.6
  */
 function superpwa_multisite_activation_status( $status ) {
@@ -48,6 +50,22 @@ function superpwa_multisite_activation_status( $status ) {
 	
 	// Set the status for the current blog.
 	$superpwa_sites[ get_current_blog_id() ] = $status;
+	
+	/**
+	 * If SuperPWA is deactivated and if the blog is using the default settings (i.e, settings not saved to database yet).
+	 * 
+	 * SuperPWA ships with smart defaults. Some blogs might not change the default settings. 
+	 * Default settings are not saved to the database until the user clicks the "Save Settings" in the plugin UI. (refer superpwa_get_settings() )
+	 * If that is the case, there is nothing more to clean up during uninstall, and we can remove the blog id from our list.
+	 * 
+	 * If settings are saved in the database, we want to preserve it, in case the user activates the plugin again. 
+	 * These settings are removed during uninstall.
+	 */
+	if ( ( $status === false ) && ( get_option( 'superpwa_settings' ) === false ) ) {
+		
+		// Remove activation status completely from the list of active sites.
+		unset( $superpwa_sites[ get_current_blog_id() ] );
+	}
 	
 	// Save it back to the database.
 	update_site_option( 'superpwa_active_sites', $superpwa_sites );
