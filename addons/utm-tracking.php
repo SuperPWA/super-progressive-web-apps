4b96f1800c86f824985fe55a4edce4e9fe87c1fd
Arun Basil Lal <arunbasillal@gmail.com>
2018-05-02 00:22:03 +0530

    UTM Tracking submenu is here

--- addons/utm-tracking.php
+++ addons/utm-tracking.php
@@ -9,3 +9,10 @@
 
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
+
+function superpwa_utm_tracking_menu_links() {
+	
+	// Add-Ons page
+	add_submenu_page( 'superpwa', __( 'Super Progressive Web Apps', 'super-progressive-web-apps' ), __( 'UTM Tracking', 'super-progressive-web-apps' ), 'manage_options', 'superpwa-utm-tracking', 'superpwa_addons_interface_render' );
+}
+add_action( 'admin_menu', 'superpwa_utm_tracking_menu_links' );
