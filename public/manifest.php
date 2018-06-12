4a01c1ef3ccf76bbccdf7b54b8ad6bd1b2ce11c6
Linas <jusys.linas@gmail.com>
2018-06-12 09:10:02 +0300

    Update manifest.php
    Has to $arg not $args singular not plural.

--- public/manifest.php
+++ public/manifest.php
@@ -33,7 +33,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  */
 function superpwa_manifest( $arg = 'src' ) {
 	
-	$manifest_filename = apply_filters( 'superpwa_manifest_filename', 'superpwa-manifest' . superpwa_multisite_filename_postfix() . '.json', $args );
+	$manifest_filename = apply_filters( 'superpwa_manifest_filename', 'superpwa-manifest' . superpwa_multisite_filename_postfix() . '.json', $arg );
 	
 	switch( $arg ) {
 		
