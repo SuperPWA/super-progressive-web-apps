ce7de3f265a055f6959003b7d3122f786f9ea3fd
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-05-09 18:47:52 +0100

    feat: setup and create the functions and variables needed

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -1,7 +1,60 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-	navigator.serviceWorker.register(superpwa_sw.url)
-	.then(function(registration) { console.log('SuperPWA service worker ready'); registration.update(); })
-	.catch(function(error) { console.log('Registration failed with ' + error); });
-  });
+	window.addEventListener('load', function () {
+		navigator.serviceWorker.register(superpwa_sw.url)
+			.then(function (registration) {
+				console.log('SuperPWA service worker ready');
+				registration.update();
+				subOnlineOfflineIndicator();
+			})
+			.catch(function (error) { console.log('Registration failed with ' + error); });
+	});
+
+	/****************** Start : Online/Offline Indicator ******************/
+
+	// Variables & default values
+	let isOffline = false,
+		goOfflineMsg = 'You\'re currently offline',
+		backOnlineMsg = 'You\'re back online <a href="javascript:location.reload()">refresh</a>';
+
+	/**
+	* Subscribe to online offline indicator
+	*/
+	function subOnlineOfflineIndicator() {
+		injectSnackbarHtml();
+		injectSnackbarCss();
+		runOnlineOfflineIndicator();
+	}
+
+	/**
+ 	* Inject html of snackbar
+ 	*/
+	function injectSnackbarHtml() {
+	}
+
+	/**
+ 	* Inject style css of snackbar
+ 	*/
+	function injectSnackbarCss() {
+	}
+
+	/**
+ 	* Show the state of the mode of connectivity to the user :  back onLine | go offLine
+ 	*/
+	function runOnlineOfflineIndicator() {
+	}
+
+	/**
+ 	* Show with the given message in the snackbar
+ 	* @param {String} msg 
+ 	*/
+	function showSnackbar(msg) {
+	}
+
+	/**
+	* Hide snackbar
+	*/
+	function hideSnackbar() {
+	}
+
+	/****************** End : Online/Offline Indicator ******************/
 }
