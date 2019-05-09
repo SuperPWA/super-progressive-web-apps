df5d22beb878ed4de9fcaff4f7246ca9a1b3a292
mohcinenazrhan <nazrhan.mohcine@gmail.com>
2019-05-09 18:57:20 +0100

    feat: implement the function Inject style css of snackbar

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -55,6 +55,82 @@ if ('serviceWorker' in navigator) {
  	* Inject style css of snackbar
  	*/
 	function injectSnackbarCss() {
+		const css = `body.snackbar--show .snackbar {
+			-webkit-transform: translateY(0);
+			transform: translateY(0); 
+		}
+
+		.snackbar {
+			box-sizing: border-box;
+			background-color: #121213;
+			color: #fff;
+			padding: 10px 55px 10px 10px;
+			position: fixed;
+			z-index: 9999999999999999;
+			left: 15px;
+			bottom: 15px;
+			border-radius: 5px 8px 8px 5px;
+			max-width: 90%;
+			min-height: 48px;
+			line-height: 28px;
+			font-size: 16px;
+			-webkit-transform: translateY(150%);
+			transform: translateY(150%);
+			will-change: transform;
+			-webkit-transition: -webkit-transform 200ms ease-in-out;
+			-webkit-transition-delay: 0s;
+					transition-delay: 0s;
+			-webkit-transition: -webkit-transform 200ms ease-in-out false;
+			transition: -webkit-transform 200ms ease-in-out false;
+			transition: transform 200ms ease-in-out false;
+			transition: transform 200ms ease-in-out false, -webkit-transform 200ms ease-in-out false; 
+		}
+
+		.snackbar p {
+			margin: 0;
+			color: #fff;
+			text-align: center; 
+		}
+
+		.snackbar .snackbar-close {
+			position: absolute;
+			top: 0;
+			right: 0;
+			width: 45px;
+			height: 100%;
+			padding: 0;
+			background: #dbb11c;
+			border: none;
+			font-size: 28px;
+			font-weight: normal;
+			border-radius: 0 5px 5px 0;
+			color: #FFF;
+			font-family: Arial, Helvetica, sans-serif;
+		}
+
+		.snackbar .snackbar-close:hover,
+		.snackbar .snackbar-close:focus {
+			background: #ddb728;
+		}
+
+		.snackbar a {
+			color: #FFF;
+			font-weight: bold;
+			text-decoration: underline; 
+		}`;
+			
+		const head = document.head || document.getElementsByTagName('head')[0];
+		const style = document.createElement('style');
+
+		style.type = 'text/css';
+		if (style.styleSheet) {
+			// This is required for IE8 and below.
+			style.styleSheet.cssText = css;
+		} else {
+			style.appendChild(document.createTextNode(css));
+		}
+
+		head.appendChild(style);
 	}
 
 	/**
