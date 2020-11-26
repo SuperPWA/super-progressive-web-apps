2ca047dd5e496a404e0904d92fa12aa87b52bb05
Ajeet <ajeet.gupta51@gmail.com>
2020-11-26 11:44:06 +0530

    update for CTA
    update for CTA

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -3,5 +3,60 @@ if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register(superpwa_sw.url)
 	.then(function(registration) { console.log('SuperPWA service worker ready'); registration.update(); })
 	.catch(function(error) { console.log('Registration failed with ' + error); });
+
+
+	var deferredPrompt;
+	window.addEventListener('beforeinstallprompt', function(e){
+		deferredPrompt = e;
+		if(deferredPrompt != null || deferredPrompt != undefined){
+			var a2hsBanner = document.getElementsByClassName("superpwa-sticky-banner");
+			if(a2hsBanner){
+				deferredPrompt.preventDefault();
+				for (var i = 0; i < a2hsBanner.length; i++) {
+					var showbanner = a2hsBanner[i].getAttribute("data-show");
+					a2hsBanner[i].style.display="flex";
+				}
+			}
+		}
+	})
+
+	window.addEventListener('appinstalled', function(evt){
+		var a2hsBanner = document.getElementsByClassName("superpwa-sticky-banner");
+		if(a2hsBanner){
+			for (var i = 0; i < a2hsBanner.length; i++) {
+				var showbanner = a2hsBanner[i].getAttribute("data-show");
+				a2hsBanner[i].style.display="none";
+			}
+		}
+	});
+	
+	var a2hsviaClass = document.getElementsByClassName("superpwa-add-via-class");
+    if(a2hsviaClass !== null){
+        for (var i = 0; i < a2hsviaClass.length; i++) {
+          a2hsviaClass[i].addEventListener("click", addToHome); 
+      }
+    }
+
+    function addToHome(){
+		if(!deferredPrompt){return ;}
+		deferredPrompt.prompt(); 
+		deferredPrompt.userChoice.then(function(choiceResult) {
+			if (choiceResult.outcome === "accepted") {
+				var a2hsBanner = document.getElementsByClassName("superpwa-sticky-banner");
+				if(a2hsBanner){
+					for (var i = 0; i < a2hsBanner.length; i++) {
+						var showbanner = a2hsBanner[i].getAttribute("data-show");
+						a2hsBanner[i].style.display="none";
+					}
+				}//a2hsBanner if
+				console.log("User accepted the prompt");
+			}else{
+				console.log("User dismissed the prompt");
+			}
+			deferredPrompt = null;
+		});
+	} // function closed addToHome
+
+
   });
 }
