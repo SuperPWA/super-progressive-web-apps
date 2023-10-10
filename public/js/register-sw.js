250b7a991e7e416074b03f945e4c345c3ee29d0e
Akshay Wali <akshaywali@live.in>
2023-10-10 13:34:18 +0530

    Push notification is not working on IOS. #468

--- public/js/register-sw.js
+++ public/js/register-sw.js
@@ -6,6 +6,11 @@ if ('serviceWorker' in navigator) {
 		{
 			registration.update(); 
 		}
+		if(firebase && typeof pushnotification_load_messaging =='function'){
+			const messaging = firebase.messaging();
+			messaging.useServiceWorker(registration);
+			pushnotification_load_messaging();
+		}
 		subOnlineOfflineIndicator();
 	})
 	.catch(function(error) { console.log('Registration failed with ' + error); });
